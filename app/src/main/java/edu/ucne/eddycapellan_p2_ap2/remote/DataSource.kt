5306dcecf0525c96d89ae3b213daef3af18b3526
Cape23180909 <samilgamer@outlook.com>
2025-06-18 22:04:26 -0400

    Terminando de hacer el DataSource

---
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/DataSource.kt
@@ -0,0 +1,11 @@
+package edu.ucne.eddycapellan_p2_ap2.remote
+
+import edu.ucne.eddycapellan_p2_ap2.remote.dto.RepositoryDto
+import javax.inject.Inject
+
+class DataSource @Inject constructor(
+    private val api: GitHubApi
+){
+    suspend fun getApi(username: String): List<RepositoryDto> = api.listRepos(username)
+
+}
