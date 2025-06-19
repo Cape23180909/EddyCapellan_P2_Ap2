69500f765311e40223404ba29ab9e08a3e82c7d9
Cape23180909 <samilgamer@outlook.com>
2025-06-18 22:06:13 -0400

    Agregando el NavHost al Main Activity

--- app/src/main/java/edu/ucne/eddycapellan_p2_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/MainActivity.kt
@@ -11,20 +11,26 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.eddycapellan_p2_ap2.presentation.ApiEjemplo.ApiViewModel
+import edu.ucne.eddycapellan_p2_ap2.presentation.navigation.ApiNavHost
 import edu.ucne.eddycapellan_p2_ap2.ui.theme.EddyCapellan_P2_Ap2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             EddyCapellan_P2_Ap2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                val ApiViewModel: ApiViewModel = hiltViewModel()
+                ApiNavHost(
+                    navHostController = navController,
+                    apiViewModel = ApiViewModel,
                     )
-                }
             }
         }
     }
