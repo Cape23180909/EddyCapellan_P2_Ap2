71162fdfe6bf2ec432a31554e28f30dc64943cf6
Cape23180909 <samilgamer@outlook.com>
2025-06-18 22:05:14 -0400

    Ternminando de hacer el GitHupApi

---
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/GitHupApi.kt
@@ -0,0 +1,10 @@
+package edu.ucne.eddycapellan_p2_ap2.remote
+
+import edu.ucne.eddycapellan_p2_ap2.remote.dto.RepositoryDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubApi {
+    @GET("users/{username}/repos")
+    fun listRepos(@Path("username") username: String): List<RepositoryDto>
+}
