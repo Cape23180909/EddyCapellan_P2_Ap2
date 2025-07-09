68b2827df6fdfadc4eac7ab10b908fc44a2e71d5
Cape23180909 <samilgamer@outlook.com>
2025-07-09 19:45:27 -0400

    Agregando los metodos Post, como el siguiente Get, que me traera esos repositorio con su contrinuidorres.

--- app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/GitHubApi.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/GitHubApi.kt
@@ -1,10 +1,22 @@
 package edu.ucne.eddycapellan_p2_ap2.remote
 
+import edu.ucne.eddycapellan_p2_ap2.remote.dto.ContribuidoreDto
 import edu.ucne.eddycapellan_p2_ap2.remote.dto.RepositoryDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface GitHubApi {
     @GET("users/{username}/repos")
     suspend fun listRepos(@Path("username") username: String): List<RepositoryDto>
+
+    @GET("repos/{owner}/{repo}/contributors")
+    suspend fun getContributors(
+        @Path("owner") owner: String,
+        @Path("repo") repo: String
+    ): List<ContribuidoreDto>
+
+    @POST("user/repos")
+    suspend fun createRepository(@Body repo: RepositoryDto): RepositoryDto
 }
