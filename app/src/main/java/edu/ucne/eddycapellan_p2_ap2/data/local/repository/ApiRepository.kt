132e9649af4d8665ff49a88e8181c5c1318fbbda
Cape23180909 <samilgamer@outlook.com>
2025-07-09 19:37:48 -0400

    Agregando un nuevo metodo al repository, para consumir todo esos contribuidores de ese repository x como el create de repository.

--- app/src/main/java/edu/ucne/eddycapellan_p2_ap2/data/local/repository/ApiRepository.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/data/local/repository/ApiRepository.kt
@@ -3,6 +3,7 @@ package edu.ucne.eddycapellan_p2_ap2.data.local.repository
 
 import edu.ucne.eddycapellan_p2_ap2.remote.GitHubApi
 import edu.ucne.eddycapellan_p2_ap2.remote.Resource
+import edu.ucne.eddycapellan_p2_ap2.remote.dto.ContribuidoreDto
 import edu.ucne.eddycapellan_p2_ap2.remote.dto.RepositoryDto
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -20,4 +21,25 @@ class ApiRepository @Inject constructor(
             emit(Resource.Error(e.localizedMessage ?: "Error al obtener los repositorios"))
         }
     }
+
+    suspend fun createRepository(repo: RepositoryDto): Flow<Resource<RepositoryDto>> = flow {
+        try {
+            emit(Resource.Loading())
+            val result = api.createRepository(repo)
+            emit(Resource.Success(result))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "Error desconocido"))
+        }
+    }
+
+    fun getContributors(owner: String, repo: String): Flow<Resource<List<ContribuidoreDto>>> = flow {
+        emit(Resource.Loading())
+        try {
+            val result = api.getContributors(owner, repo)
+            emit(Resource.Success(result))
+        } catch (e: Exception) {
+            emit(Resource.Error(e.localizedMessage ?: "Error al obtener contribuyentes"))
+        }
+    }
+
 }
