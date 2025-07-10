17a14dda78513030d609654e5790ed1ed94af49c
Cape23180909 <samilgamer@outlook.com>
2025-07-09 23:38:55 -0400

    Removiendo esta propiedad, ya que se ocurrio otra forma deimplementar el mismo.

--- app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/dto/RepositoryDto.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/dto/RepositoryDto.kt
@@ -5,6 +5,5 @@ import com.squareup.moshi.Json
 data class RepositoryDto(
     @Json(name = "name") val name: String,
     @Json(name = "description") val description: String?,
-    @Json(name = "html_url") val htmlUrl: String,
-    val propietario: PropietarioDto?
+    @Json(name = "html_url") val htmlUrl: String
 )
