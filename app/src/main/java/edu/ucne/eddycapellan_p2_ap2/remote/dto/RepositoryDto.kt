82c90b28bf5719f300ff06edc41e0bd846c09737
Cape23180909 <samilgamer@outlook.com>
2025-07-09 21:23:51 -0400

    Agregando nueva propiedad al Dto de repository

--- app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/dto/RepositoryDto.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/dto/RepositoryDto.kt
@@ -5,5 +5,6 @@ import com.squareup.moshi.Json
 data class RepositoryDto(
     @Json(name = "name") val name: String,
     @Json(name = "description") val description: String?,
-    @Json(name = "html_url") val htmlUrl: String
+    @Json(name = "html_url") val htmlUrl: String,
+    val propietario: PropietarioDto?
 )
