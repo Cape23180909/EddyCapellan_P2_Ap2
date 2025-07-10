a49df9b3a518471334eefaaf0f124e61c9194289
Cape23180909 <samilgamer@outlook.com>
2025-07-09 23:36:12 -0400

    Terminando de hacer el ApiUiSate.

---
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/presentation/ApiEjemplo/ApiUiState.kt
@@ -0,0 +1,13 @@
+package edu.ucne.eddycapellan_p2_ap2.presentation.ApiEjemplo
+
+import edu.ucne.eddycapellan_p2_ap2.remote.dto.RepositoryDto
+
+data class ApiUiState(
+    val name: String = "",
+    val description: String = "",
+    val htmlUrl: String = "",
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val api: List<RepositoryDto> = emptyList(),
+    val inputError: String? = null
+)
