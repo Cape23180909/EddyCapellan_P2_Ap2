3b18a0e803b1db325965110509c430ed8c568a88
Cape23180909 <samilgamer@outlook.com>
2025-07-09 23:35:45 -0400

    Modificando los metodos y removiendo lo que no seran utilizados en el mismo. y quitando los parametros el repositorypara que llegue automatico.

--- app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/DataSource.kt
+++ app/src/main/java/edu/ucne/eddycapellan_p2_ap2/remote/DataSource.kt
@@ -1,16 +1,27 @@
 package edu.ucne.eddycapellan_p2_ap2.remote
 
+import edu.ucne.eddycapellan_p2_ap2.remote.dto.ContribuidoreDto
 import edu.ucne.eddycapellan_p2_ap2.remote.dto.RepositoryDto
 import javax.inject.Inject
 
 class DataSource @Inject constructor(
     private val api: GitHubApi
 ){
-    suspend fun listRepos(username: String): List<RepositoryDto> {
-        return api.listRepos(username)
+    suspend fun listRepos(): List<RepositoryDto> {
+        val response = api.listRepos()
+        if(response.isSuccessful) {
+            return response.body() ?: emptyList()
+        } else {
+            throw Exception("Error: ${response.code()} ${response.message()}")
+        }
     }
 
-    suspend fun createRepository(repositoryDto: RepositoryDto): RepositoryDto {
-        return api.createRepository(repositoryDto)
+    suspend fun getContributors(owner: String, repo: String): List<ContribuidoreDto> {
+        val response = api.getContributors(owner, repo)
+        if(response.isSuccessful) {
+            return response.body() ?: emptyList()
+        } else {
+            throw Exception("Error: ${response.code()} ${response.message()}")
+        }
     }
 }
